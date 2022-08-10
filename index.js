94f779d33d1b013e41286f941f86bd56d7418f98
Noah King <noahking123@yahoo.com>
2022-08-10 11:37:58 -0400

    commit tree dcd32f9b254885036914022d11452b8b6028df51

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Noah',
+    streetAddress: 'Home'
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...obj};
+    console.log(newEmployee)
+    console.log([key])
+    console.log(value)
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee.streetAddress = '12 Broadway';
+    return employee;
+}
+
+/*function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    console.log(newEmployee)
+    delete newEmployee[key]
+    //newEmployee.name = 'Sam';
+    console.log(newEmployee);
+    return newEmployee;
+}
+const newEmployee = {
+    name: 'Tod',
+    streetAddress: 'Home'
+}
+deleteFromEmployeeByKey(newEmployee,'streetAddress');
+*/
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+
+}
